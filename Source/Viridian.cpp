be14d53badfa97645660368d4b195add1855de1e
Jasper Drescher <jasperdre@gmail.com>
2022-07-28 00:27:13 +0200

    Change texture parameters

--- Source/Viridian.cpp
+++ Source/Viridian.cpp
@@ -113,10 +113,10 @@ int CreateTexture(const std::string& aFilepath)
 	glGenTextures(1, &textureIdentifier);
 	glBindTexture(GL_TEXTURE_2D, textureIdentifier);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
 	stbi_set_flip_vertically_on_load(1);
 
@@ -133,6 +133,7 @@ int CreateTexture(const std::string& aFilepath)
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 	glGenerateMipmap(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, 0);
+
 	stbi_image_free(data);
 
 	return static_cast<int>(textureIdentifier);
